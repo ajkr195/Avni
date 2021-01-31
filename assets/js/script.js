c1739062a5cbe4b7a916e1ccd5167cab9b8079fe
ajkr195 <ajkr195@gmail.com>
2021-01-31 15:54:46 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,3 +224,32 @@ function changeColor(colornumber) {
 }
 
 changeColor('colornumber');
+
+
+var dropdown = document.getElementsByClassName("sidebar-dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+	dropdown[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var dropdownContent = this.nextElementSibling;
+		if (dropdownContent.style.display === "block") {
+			dropdownContent.style.display = "none";
+		} else {
+			dropdownContent.style.display = "block";
+		}
+	});
+}
+
+function myFunction() {
+	var element = document.getElementById("sidenavicon");
+	element.classList.toggle("sidenaviconopen");
+}
+
+function openOverlayNav() {
+	document.getElementById("sidebarOverlayNav").style.width = "100%";
+}
+
+function closeOverlayNav() {
+	document.getElementById("sidebarOverlayNav").style.width = "0%";
+}
